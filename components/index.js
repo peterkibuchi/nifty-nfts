e6f53de1e613ed9b95df540639eebd9e7f5ff797
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2022-08-26 08:28:27 +0300

    Complete Components

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { SubInfo } from "./SubInfo";
+
+export {
+  CircleButton,
+  DetailsBid,
+  DetailsDesc,
+  FocusedStatusBar,
+  HomeHeader,
+  NFTCard,
+  RectButton,
+  SubInfo,
+};
